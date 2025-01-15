695ba521d56b69bbc48e9f7c8324c5668b098d85
Stephen O'Dwyer <67125983+gradientsearch@users.noreply.github.com>
2025-01-15 09:46:53 -0500

    prepare py script

--- scripts/emoji_service.py
+++ scripts/emoji_service.py
@@ -1,8 +1,13 @@
 import requests
 import json 
 import os
-
-
+import unicodeMap
+class Emoji:
+    unicode: str 
+    category: str
+    subcategory: str
+    description: str
+    emoji: str
 
 def getEmojiList():
     
@@ -19,4 +24,87 @@ def printEmoji():
         f.flush()
         os.fsync(f.fileno())
         
-printEmoji()
+def buildUnicodeMap(): 
+    list1 = []
+    with open('scripts/full-emoji-list.txt', 'r') as file:
+        list1 = file.readlines()
+    
+    
+    with open('scripts/unicodeMap.py', 'w') as f:
+        
+        f.write(" UNICODE_MAP = {\n")
+        
+        for l in list1:
+            l = l.strip()
+            if len(l) == 0:
+                continue
+            
+            if l.startswith('@@'):
+                currentCategory=l.replace('@@','').lower()
+                continue
+            if l.startswith('@'):
+                currentSubcatgory = l.replace('@','').lower()
+                continue
+            
+            
+            
+            ls = l.split('\t')
+
+            
+            
+            for v in ls[0].upper().split(' '):
+                #if len()
+                code = f"\\U000{v}"
+                f.write(f'"{v}": "{code}",\n')
+        f.write("}")
+
+def buildEmojiMap(): 
+    list1 = []
+    with open('scripts/full-emoji-list.txt', 'r') as file:
+        list1 = file.readlines()
+    
+    list2 = []
+    with open('scripts/emojis.json', 'r') as file:
+        list2 = json.load(file)
+    #print(list)
+    #print(list2)
+    
+    currentCategory: str
+    currentSubcatgory: str
+
+    
+    for l in list1:
+        l = l.strip()
+        if len(l) == 0:
+            continue
+        
+        if l.startswith('@@'):
+            currentCategory=l.replace('@@','').lower()
+            continue
+        if l.startswith('@'):
+            currentSubcatgory = l.replace('@','').lower()
+            continue
+        
+        
+        
+        ls = l.split('\t')
+
+        
+        
+        for v in ls[0].upper().split(' '):
+            code = unicodeMap.UNICODE_MAP[v]
+            print(code)
+        
+        desc = ls[1]
+        
+        
+      #  print((code, desc))
+        
+
+
+def main():
+    buildEmojiMap()
+    #buildUnicodeMap()
+
+if __name__ == '__main__':
+    main()

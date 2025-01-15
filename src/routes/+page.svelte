b08802d5de3de2aababfa85dc623b043ba9ce4e9
Stephen O'Dwyer <67125983+gradientsearch@users.noreply.github.com>
2025-01-15 13:31:31 -0500

    refine layout. add copy to clipboard and toast

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { getData } from './services/emoji.service';
+	import { SvelteMap } from 'svelte/reactivity';
 
 	let emojis: any = $state(undefined);
+	let toasts: Map<string, any> = $state(new SvelteMap<string, any>());
 	onMount(async () => {
 		let [resp, err] = await getData();
 		if (err !== undefined) {
@@ -11,6 +13,23 @@
 			emojis = JSON.parse(resp);
 		}
 	});
+
+	function copy(emoji: any) {
+		navigator.clipboard.writeText(emoji['emoji']).then(
+			() => {
+				let id = crypto.randomUUID();
+
+				toasts.set(id, emoji);
+				setTimeout(() => {
+					toasts.delete(id);
+				}, 2000);
+			},
+			() => {
+				// Failed to copy text
+				console.error('Failed to copy text');
+			}
+		);
+	}
 </script>
 
 <header class="bg-base-100 sticky top-0 flex h-[48px] items-center justify-center p-1">
@@ -22,17 +41,43 @@
 </header>
 
 <div class="flex h-full w-full justify-center">
-	<div class="w-full max-w-2xl lg:max-w-5xl">
+	<div class="w-full max-w-2xl lg:max-w-5xl lg:text-2xl">
 		{#if emojis}
-        {#each Object.keys(emojis) as category}
-            <div>
-                <h1 class="font-bold">{category}</h1>
-                {#each Object.keys(emojis[category]) as subcategory}
-                <h2> {subcategory}</h2>
-                {/each}
-            </div>
-            {/each}
-			{JSON.stringify(emojis)}
+			{#each Object.keys(emojis) as category}
+				<div class="mx-4 mt-10">
+					<h1 class="font-bold capitalize">{category}</h1>
+					{#each Object.keys(emojis[category]) as subcategory}
+						<div class="py-4">
+							<h2 class="capitalize">{subcategory.replaceAll('-', ' ')}</h2>
+						</div>
+						<div class="flex flex-wrap lg:text-3xl">
+							{#each Object.keys(emojis[category][subcategory]) as emoji}
+								<button
+									onclick={() => {
+										copy(emojis[category][subcategory][emoji]);
+									}}
+									class="flex flex-wrap border p-4"
+								>
+									<span>
+										{emojis[category][subcategory][emoji]['emoji']}
+									</span>
+								</button>
+							{/each}
+						</div>
+					{/each}
+				</div>
+			{/each}
 		{/if}
 	</div>
 </div>
+
+{#each toasts.keys() as t}
+	<aside
+		class=" bg-base-900 text-base-200 fixed bottom-4 end-4 z-50 flex items-center justify-center gap-4 rounded-lg px-5 py-3"
+	>
+		<p>
+			Copied {toasts.get(t)['name']}
+			{toasts.get(t)['emoji']}!
+		</p>
+	</aside>
+{/each}
